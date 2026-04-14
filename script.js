1ddd6fcc9a78d969f9680d5c7f03b94a85529618
MARVER1X <bbmardd@gmail.com>
2026-04-14 21:45:11 +0100

    fixes to JS time update and HTML semantics

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const timeElement = document.getElementById("time");
 
 function updateTime() {
-  timeElement.textContent = Date.now();
+  const now = Date.now();
+  timeElement.textContent = now;
+  timeElement.setAttribute("datetime", now);
 }
 
 // Initial render
 updateTime();
 
-// Update every second
 setInterval(updateTime, 1000);
